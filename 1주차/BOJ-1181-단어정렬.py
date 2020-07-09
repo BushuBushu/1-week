68a853a60aabc5274e32fb85b21062448a8b1147
LeeYeonJu <yeonzzu2@gmail.com>
2020-07-09 15:51:55 +0900

    solve BOJ-1181-단어정렬 by dictionary

--- 1주차/BOJ-1181-단어정렬.py
+++ 1주차/BOJ-1181-단어정렬.py
@@ -1,1 +1,15 @@
-# my til
+
+wordDict = {}
+for _ in range(int(input())):
+    word = input()
+    if word not in wordDict.values():
+        if len(word) not in wordDict.keys():
+            wordDict[len(word)] = {word}
+        else:
+            wordDict[len(word)].add(word)
+for key, value in wordDict.items():
+    wordDict[key] = sorted(list(value))
+
+for idx, words in sorted(wordDict.items()):
+    for word in words:
+        print(word)
