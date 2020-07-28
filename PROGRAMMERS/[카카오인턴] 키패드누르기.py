733d0dc7aa536db879ce05ceea4005487f527081
joylish <yeonzzu2@gmail.com>
2020-07-28 23:57:26 +0900

    업데이트 [카카오인턴] 키패드 누르기

--- PROGRAMMERS/[카카오인턴] 키패드누르기.py
+++ PROGRAMMERS/[카카오인턴] 키패드누르기.py
@@ -1,8 +1,8 @@
 keypads= {1:(0,0), 2:(0,1),3:(0,2),4:(1,0),5:(1,1),6:(1,2), 7:(2,0),8:(2,1),9:(2,2),'*':(3,0), 0:(3,1), '#':(3,2)}
 
-def findHand(number, right, left, hand):
-    rightDist = abs(keypads[number][0] - keypads[right][0]) + abs(keypads[number][1] - keypads[right][1])
-    leftDist = abs(keypads[number][0] - keypads[left][0]) + abs(keypads[number][1] - keypads[left][1])
+def findHand(number, rightHand, leftHand, hand):
+    rightDist = abs(keypads[number][0] - keypads[rightHand][0]) + abs(keypads[number][1] - keypads[rightHand][1])
+    leftDist = abs(keypads[number][0] - keypads[leftHand][0]) + abs(keypads[number][1] - keypads[leftHand][1])
     if rightDist == leftDist:
         if hand == 'right':
             return 'R'
@@ -15,19 +15,17 @@ def findHand(number, right, left, hand):
 
 def solution(numbers, hand):
     answer = ''
-    right, left = '#', '*'
+    movedHand, rightHand, leftHand = '', '#', '*'
     for number in numbers:
-        if  number % 3 == 2 or number == 0:
-            movedHand = findHand(number, right, left, hand)
-            answer +=  movedHand
-            if movedHand == 'R':
-                right = number
-            elif movedHand =='L':
-                left = number
-        elif number % 3 == 1:
-            answer += 'L'
-            left = number
+        if number in [3, 6, 9]:
+            movedHand = 'R'
+        elif number in [1, 4, 7]:
+            movedHand = 'L'
         else:
-            answer += 'R'
-            right = number
+            movedHand = findHand(number, rightHand, leftHand, hand)
+        if movedHand == 'R':
+            rightHand = number
+        elif movedHand == 'L':
+            leftHand = number
+        answer += movedHand
     return answer
