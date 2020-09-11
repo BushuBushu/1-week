7c60dcd1964da23d797c0b473be11cacd0822035
Joylish <yeonzzu2@gmail.com>
2020-09-12 01:59:56 +0900

    코드 이상 수정

--- PROGRAMMERS/문자열 내마음대로 정렬하기.js
+++ PROGRAMMERS/문자열 내마음대로 정렬하기.js
@@ -1,11 +1,10 @@
-console.log('1111')
 function solution(strings, n) {
   const answer = strings;
   answer.sort((a,b)=>{
       if (a[n]== b[n]){
         return a < b? -1: 1;
       }
-      return a[n] < b[n]? -1: (a[n] > b[n]? 1: 0);
+      return a[n] < b[n]? -1: 1;
   })
   return answer;
 }
