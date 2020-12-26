443b5d887f188a767f0f019717c10e6b9e0eb6a3
LeeYeonJu <yeonzzu2@gmail.com>
2020-12-26 15:21:08 +0900

    FIX : test 코드 삭제

--- Inflearn/basic/01.K번째 약수/answer.py
+++ Inflearn/basic/01.K번째 약수/answer.py
@@ -1,7 +1,5 @@
-import glob
 import sys
 import math
-from unittest import TestCase, main, TestSuite
 
 
 def solution(n, k):
@@ -16,26 +14,8 @@ def solution(n, k):
     print(divisors[k - 1])
 
 
-def Test(TestCase):
-    def setUp(self):
-        _in = glob.glob('in*.txt')
-        _out = glob.glob('out*.txt')
-        for file in _in:
-            with open(file, 'r') as f:
-                self._input.append(f.read())
-        for file in _out:
-            with open(file, 'r') as f:
-                self._output.append(f.read())
-        # self.n, self.k = open("data/data.txt", "rt")
-
-    def test_solution(self):
-        # sys.stdin = open("data/in1.txt", "rt")
-        n, k = 8, 4
-        self.assertEqual(solution(n, k), 2)
-
 
 if __name__ == "__main__":
-    # sys.stdin = open("data/data.txt", "rt")
-    # n, k = map(int, input().split())
-    # solution(n, k)
-    main()
+    sys.stdin = open("data/data.txt", "rt")
+    n, k = map(int, input().split())
+    solution(n, k)
--- Inflearn/basic/02.K번째 수/answer.py
+++ Inflearn/basic/02.K번째 수/answer.py
@@ -1,7 +1,5 @@
-import glob
 import sys
 import math
-from unittest import TestCase, main, TestSuite
 
 
 def solution(idx):
@@ -12,27 +10,8 @@ def solution(idx):
     print(f'#{idx} {cut_num[k-1]}')
 
 
-def Test(TestCase):
-    def setUp(self):
-        _in = glob.glob('in*.txt')
-        _out = glob.glob('out*.txt')
-        for file in _in:
-            with open(file, 'r') as f:
-                self._input.append(f.read())
-        for file in _out:
-            with open(file, 'r') as f:
-                self._output.append(f.read())
-        # self.n, self.k = open("data/data.txt", "rt")
-
-    def test_solution(self):
-        # sys.stdin = open("data/in1.txt", "rt")
-        n, k = 8, 4
-        self.assertEqual(solution(n, k), 2)
-
-
 if __name__ == "__main__":
     sys.stdin = open("data/in1.txt", "rt")
     tcs = int(input())
     for idx in range(1, tcs+1):
         solution(idx)
-    # main()
