028a5411d87c3b936518054bf335adb769512e7e
LeeYeonJu <yeonzzu2@gmail.com>
2020-12-26 15:05:18 +0900

    DOCS : python °ü·Ã template ¼öÁ¤

--- templates/python/programmers/answer.py
+++ templates/python/programmers/answer.py
@@ -9,7 +9,7 @@ class Test(TestCase):
       self.assertEqual(solution(), 3)
 
 if __name__ == "__main__":
-  r1 = open('data/input.txt', mode='rt', encoding='utf-8')
+  r1 = open('data/data.txt', mode='rt', encoding='utf-8')
   r2 = open('data/output.txt', mode='rt', encoding='utf-8')
   
   _inputs = r1.readlines()
