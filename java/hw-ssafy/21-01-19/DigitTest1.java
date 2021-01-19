a92c7edda3e639baba642e0eb0bc32f8974f3ebf
LeeYeonJu <yeonzzu2@gmail.com>
2021-01-19 12:48:15 +0900

    SOLVE : 21-01-19 hw02-java-ssafy What - 십의자리수 빈도 구하기: Array 이용하기

---
+++ java/hw-ssafy/21-01-19/DigitTest1.java
@@ -0,0 +1,41 @@
+package com.java.algo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class DigitTest1 {
+
+	public static void main(String[] args) {
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int[] count = new int[10]; 
+			while (st.hasMoreTokens()) {
+				int val = Integer.parseInt(st.nextToken());
+				if (val != 0) {
+					count[val/10] += 1;	
+				}
+			}
+			System.out.println(toString(count));
+		}catch(IOException e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static String toString(int[] count) {
+		StringBuffer sb = new StringBuffer();
+		for(int i = 0; i < count.length; i++) {
+			if (count[i] > 0) {
+				sb.append(i);
+				sb.append(" : ");
+				sb.append(count[i]);
+				sb.append("개\n");
+			}
+		}
+		return sb.toString();
+	}
+
+}
