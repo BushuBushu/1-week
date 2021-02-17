6cd9c096976ce7d98b0482eb64b5444746fc6a21
LeeYeonJu <yeonzzu2@gmail.com>
2021-02-18 04:28:32 +0900

    code : prg 42885 greedy 구명보트

---
+++ Programmers/lv2/42885_greedy/answer.js
@@ -0,0 +1,17 @@
+function solution(people, limit) {
+  people.sort((a, b) => a - b); // 그냥 sort하면 2자리 이상일 때 정렬안됨
+
+  let count = 0;
+  let l = 0;
+  let r = people.length - 1;
+  while (l <= r) {
+    if (people[l] + people[r] <= limit) l++;
+    count++;
+    r--;
+  }
+  return count;
+}
+
+console.log(solution([40, 50, 60, 90], 90));
+// console.log(solution([70, 50, 80, 50], 100));
+// console.log(solution([50, 50, 50, 50], 100));
